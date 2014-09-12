a2db73ef1ece7b8012f7333167235a5e2de007bf
Chakib <c@fasterize.com>
2014-09-12 19:21:39 +0200

    v0.1 fist init

---
+++ prepare-commit-message.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Automatically add branch name and branch description to every commit message except merge commit.
+#
+
+COMMIT_EDITMSG=$1
+
+addBranchName() {
+  NAME=$(git branch | grep '*' | sed 's/* //')
+  DESCRIPTION=$(git config branch."$NAME".description)
+  echo "[Fasterize/$NAME] $(cat $COMMIT_EDITMSG)" > $COMMIT_EDITMSG
+
+}
+
+MERGE=$(cat $COMMIT_EDITMSG|grep -i 'merge'|wc -l)
+
+if [ $MERGE -eq 0 ] ; then
+  addBranchName
+fi
